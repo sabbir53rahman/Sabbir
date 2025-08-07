569132639f612448b11cb52331b661a4ccf94af3
sabbir53rahman <sabbir53rahman@gmail.com>
2025-08-07 10:58:03 +0600

    dsfg

--- components/LenisProvider/LenisProvider.tsx
+++ components/LenisProvider/LenisProvider.tsx
@@ -18,7 +18,7 @@ export default function LenisProvider({
 
   useEffect(() => {
     const lenis = new Lenis({
-      duration: 0.3, // Adjust this for feel; lower = faster, higher = slower
+      duration: 0.8, 
       easing: (t) => Math.min(1, 1.001 - Math.pow(2, -10 * t)), // smooth easing
       orientation: "vertical",
       gestureOrientation: "vertical",
